0813e5b3dd679652f438fc9d84ef05a26248e796
Samuel Andrés <samuel.andres@d3sistemas.com.ar>
2023-05-16 19:14:47 -0300

    Add crud to client controller

--- src/main/java/com/trantorinc/springbootlocaldevdocker/controller/ClientController.java
+++ src/main/java/com/trantorinc/springbootlocaldevdocker/controller/ClientController.java
@@ -3,9 +3,12 @@ package com.trantorinc.springbootlocaldevdocker.controller;
 import com.trantorinc.springbootlocaldevdocker.jpa.ClientRepository;
 import com.trantorinc.springbootlocaldevdocker.model.Client;
 import lombok.AllArgsConstructor;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -20,9 +23,38 @@ public class ClientController {
         return ResponseEntity.ok(clientRepository.findAll());
     }
 
+    @GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    private ResponseEntity<Client> getClientById(@PathVariable Long id) {
+        Client client = clientRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        "No client where found with the specified ID"));
+        return new ResponseEntity<Client>(client, HttpStatus.OK);
+    }
+
     @PostMapping(path = "/", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     private ResponseEntity<Client> create(@RequestBody Client client) {
         var createdClient = clientRepository.save(client);
         return ResponseEntity.ok(createdClient);
+    }
+
+    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Client> updateClient(@PathVariable Long id, @RequestBody Client client) {
+        Client updatedClient = clientRepository.findById(id)
+                .map(c -> {
+                    c.setName(client.getName());
+                    c.setSurname(client.getSurname());
+                    c.setDni(client.getDni());
+                    return clientRepository.save(c);
+                })
+                .orElseThrow(
+                        () -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                                "No client with specified ID were found"));
+        return new ResponseEntity<Client>(updatedClient, HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> deleteClient(@PathVariable Long id) {
+        clientRepository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
